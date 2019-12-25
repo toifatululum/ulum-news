7fe2177bd6604c1d2dcd0c11c0a75483bc087811
Toto <toifatululum222@gmail.com>
2019-12-25 22:44:58 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import PostList from "./components/PostList";
+import React from 'react';
 
+import './App.css';
+import PostList from './components/PostList';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Berita from './components/Berita';
 function App() {
   return (
-    <div className="App">
-      <PostList />
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <PostList />
+          </Route>
+          <Route exact path='/berita' component={Berita}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
